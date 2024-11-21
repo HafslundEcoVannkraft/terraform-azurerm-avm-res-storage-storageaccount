ee2b0a90d2fd1782fb9a47a53c31beb20e917af6
Espen Hermansen <espen.hermansen@gmail.com>
2024-11-21 14:28:22 +0100

    Remove version constraint

--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,9 @@ terraform {
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.14.0, < 2.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.114.0, < 4.0.0"
     }
     modtm = {
       source  = "Azure/modtm"
