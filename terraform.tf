cfe0ccc763aa504c677548559503b497499919bc
Espen Hermansen <espen.hermansen@gmail.com>
2024-11-28 14:01:58 +0100

    Remove constraints

--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,9 @@ terraform {
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.14.0, < 3.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.116.0, < 4.0.0"
     }
     modtm = {
       source  = "Azure/modtm"
